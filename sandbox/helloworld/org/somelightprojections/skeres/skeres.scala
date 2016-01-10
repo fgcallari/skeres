7bd7598c77c801e4a70435310a66bc15a748b549
fgcallari <fgcallari@gmail.com>
2016-01-09 22:31:54 -0500

    Update skeres.scala

--- sandbox/helloworld/org/somelightprojections/skeres/skeres.scala
+++ sandbox/helloworld/org/somelightprojections/skeres/skeres.scala
@@ -1,32 +1,29 @@
 package org.somelightprojections.skeres
-import ceres._
+
+import com.google.ceres._
 
 package object skeres {
   // Readable renaming of SWIG-generated types.
   type DoublePointerPointer = SWIGTYPE_p_p_double
   type DoublePointer = SWIGTYPE_p_double
-  type LossFunctionPointer = SWIGTYPE_p_ceres__LossFunction
 
   // Implicit conversions betwee pointer objects and
   // arrays or matrices.
-  implicit def doublePointerPointerToBlockMatrix(p: DoublePointerPointer) = 
+  implicit def doublePointerPointerToBlockMatrix(p: DoublePointerPointer) =
     BlockMatrix(p)
 
-  implicit def doublePointerToRichDoubleArray(p: DoublePointer) = 
+  implicit def doublePointerToRichDoubleArray(p: DoublePointer) =
     RichDoubleArray(DoubleArray.frompointer(p))
 
-  implicit def doubleArraytoRichDoubleArray(a: DoubleArray) = 
+  implicit def doubleArraytoRichDoubleArray(a: DoubleArray) =
      RichDoubleArray(a)
-
-  // Trivial loss function (we can't use null's directly).
-  lazy val NullLossFunction: LossFunctionPointer = new TrivialLossFunction
 }
 
 import skeres._
 
 // Enrichment of carrays.i's DoubleArray to
 // get more convenient access and conversion
-// back to pointer and to scala arrays. 
+// back to pointer and to scala arrays.
 case class RichDoubleArray(a: DoubleArray) {
   def get(i: Int): Double = a.getitem(i)
 
@@ -34,9 +31,12 @@ case class RichDoubleArray(a: DoubleArray) {
 
   def toPointer: DoublePointer = a.cast
 
+  // This method is very inefficient. Prefer using the get/set methods above in cost function
+  // callbacks.
+  // Use of this method is recommended only to return to Scala the result of the optimization.
   def toArray(length: Int): Array[Double] = {
     val aScala = Array.ofDim[Double](length)
-    ArrayHelper.pointerToArray(a.toPointer, length, aScala)
+    aScala.indices.foreach(i => aScala(i) = a.getitem(i))
     aScala
   }
 }
@@ -48,12 +48,9 @@ case class BlockMatrix(data: DoublePointerPointer) {
      BlockMatrixHelper.row(data, i)
   }
   def get(i: Int, j: Int) = doubleArrayRow(i).getitem(j)
-  def set(i: Int, j: Int, x: Double) = doubleArrayRow(i).setitem(j, x)  
+  def set(i: Int, j: Int, x: Double) = doubleArrayRow(i).setitem(j, x)
 }
 object BlockMatrix {
   @inline def isNull(data: DoublePointerPointer) =
     BlockMatrixHelper.isNull(data)
 }
-
-// Extension of LossFunction* that mimics a null pointer. 
-class TrivialLossFunction extends LossFunctionPointer
