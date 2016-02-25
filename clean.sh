d188055372a1bef890b490c90ea033f1a36cdfa7
Franco Callari <francesco@twitter.com>
2016-02-25 15:00:53 -0800

    cleanup

--- clean.sh
+++ clean.sh
@@ -5,5 +5,8 @@ set -eux
 . ./configuration.sh
 
 rm -rf project root target src \
-core/target examples/lib examples/target \
+core/target core/project \
+examples/lib examples/target examples/project \
+examples/src/main/java examples/src/main/resources examples/src/main/scala-2.11 \
+examples/src/test \
 $GEN_JAVA_DIR $GEN_CPP_DIR $GEN_OBJ_DIR $GEN_LIB_DIR
