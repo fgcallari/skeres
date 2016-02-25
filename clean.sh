6af821d8b6b2f69d4438890b4f2c85a80b9544cb
Franco Callari <francesco@twitter.com>
2016-02-25 15:05:31 -0800

    wip

--- clean.sh
+++ clean.sh
@@ -6,7 +6,10 @@ set -eux
 
 rm -rf project root target src \
 core/target core/project \
+core/src/main/c++ core/src/main/java \
+core/src/main/resources core/src/main/scala-2.11 \
+core/src/test/java core/src/test/scala-2.11 \
 examples/lib examples/target examples/project \
-examples/src/main/java examples/src/main/resources examples/src/main/scala-2.11 \
-examples/src/test \
+examples/src/main/java examples/src/main/resources \
+examples/src/main/scala-2.11 examples/src/test \
 $GEN_JAVA_DIR $GEN_CPP_DIR $GEN_OBJ_DIR $GEN_LIB_DIR
