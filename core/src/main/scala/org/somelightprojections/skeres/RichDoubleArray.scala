f11b28b8941240d6c51511c9b61ed49fe9e22850
Franco Callari <francesco@twitter.com>
2016-03-07 22:41:59 -0500

    wip

--- core/src/main/scala/org/somelightprojections/skeres/RichDoubleArray.scala
+++ core/src/main/scala/org/somelightprojections/skeres/RichDoubleArray.scala
@@ -3,14 +3,34 @@ package org.somelightprojections.skeres
 import com.google.ceres.{DoubleArraySlice, DoubleArray}
 import spire.implicits._
 
-// Enrichment of SWIG carrays.i's DoubleArray to
-// get more convenient access and conversion
-// back to pointer and to scala arrays.
+/**
+  * Enrichment of SWIG carrays.i's DoubleArray to get more convenient access and conversion
+  * back to pointer and to scala arrays.
+  * Memory is owned by the native code, and there is NO BOUND CHECKING: heap corruption
+  * may occur if invalid indices are specified.
+  */
 case class RichDoubleArray(a: DoubleArray) {
+  /**
+    * Returns a native array element.
+    * @param i element index.
+    * @return Native array element at i.
+    */
   def get(i: Int): Double = a.getitem(i)
 
+  /**
+    * Assigns a native array element.
+    * @param i element index.
+    * @param x value to assign.
+    */
   def set(i: Int, x: Double): Unit = a.setitem(i, x)
 
+  /**
+    * Assign the first from.length elements of a native array.
+    * WARNING: heap corruption will occurr if the array argument has length larger than
+    * the native array.
+    * @param from Array to copy from.
+    * @return The underlying (native) DoubleArray.
+    */
   def copyFrom(from: Array[Double]): DoubleArray = {
     cforRange(0 until from.length)(i => a.setitem(i, from(i)))
     a
